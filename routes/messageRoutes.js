096a2498059eca9d874dd59e0af8321692c6e025
Jash Agrawal <jash.a@vsureconsultancy.com>
2025-03-04 15:58:46 +0530

    ```
     Mar 4, 2025 3:58 PM: ```
    chore(db): Remove database connection and conversation models
    
    - Removed all database connection code.
    - Removed Conversation Model.
    - Removed Conversation routes.
    - Removed Message Model.
    - Removed Message routes.
    - Removed User Model.
    - Removed User routes.
    
    Reason for Changes:
    
    The project's database is not currently in use. This commit removes the redundant database connection and models to simplify the project and avoid unnecessary complexity. Removing these models also removes their related routes, which are no longer needed.
    ```
    
     Automatic commit by Commit-o-Clock

--- routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const messageController_1 = require("../controllers/messageController");
-const router = (0, express_1.Router)();
-router.post('/', messageController_1.sendMessage);
-router.get('/:conversationId', messageController_1.getMessageHistoryByConversationId);
-exports.default = router;
--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.post('/', userController_1.createUser);
-router.get('/', userController_1.getAllUsers);
-router.put('/:userId', userController_1.updateUser);
-exports.default = router;
