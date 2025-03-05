6a26ec536f9c74cab75b011a4e6f824fd0dd3fd9
Jash Agrawal <jash.a@vsureconsultancy.com>
2025-03-05 18:38:37 +0530

    fix errors

--- controllers/conversationController.ts
+++ controllers/conversationController.ts
@@ -1,40 +1,53 @@
-import { Request, Response } from 'express';
-import Conversation from '../models/Conversation';
-import User from '../models/User';
+import { Request, Response } from "express";
+import Conversation from "../models/Conversation";
+import User from "../models/User";
 
 export const createConversation = async (req: Request, res: Response) => {
   try {
     const { participants } = req.body;
-    const conversation = new Conversation({ participants });
-    await conversation.save();
+    const users = await User.find({ myServerUserId: { $in: participants } });
+    const userIds = users.map((user) => user._id);
+    const conversation = await Conversation.findOne({
+      participants: { $all: userIds },
+    });
+    if (conversation) {
+      res.status(200).json(conversation);
+      return;
+    }
+    const newConversation = new Conversation({ participants: userIds });
+    await newConversation.save();
     res.status(201).json(conversation);
   } catch (error) {
-    res.status(500).json({ error: 'Error creating conversation' });
+    res.status(500).json({ error: "Error creating conversation" });
   }
 };
 
 export const getConversationsByUserId = async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
-    const user = await User
-      .findOne({ myServerUserId: userId});
-      if(!user) {
-        res.status(404).json({ error: 'User not found' });
-        return;
-      }
+    const user = await User.findOne({ myServerUserId: userId });
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
     const populatedConversations = await Conversation.find({
-      participants:userId,
-    }).populate('participants').sort({ createdAt: -1 })
-    const conversationsWithRecivers = populatedConversations.map((conversation) => {
-      const newConversation:any = conversation.toObject();
-      newConversation.reciver = conversation.participants.find((participant) => participant.toString() !== (""+user._id));
-      return newConversation;
+      participants: user._id,
     })
-
+      .populate("participants")
+      .sort({ createdAt: -1 });
+    const conversationsWithRecivers = populatedConversations.map(
+      (conversation) => {
+        const newConversation: any = conversation.toObject();
+        newConversation.reciver = conversation.participants.find(
+          (participant) => participant.toString() !== "" + user._id
+        );
+        return newConversation;
+      }
+    );
 
     res.status(200).json(conversationsWithRecivers);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Error fetching conversations' });
+    console.log(error);
+    res.status(500).json({ error: "Error fetching conversations" });
   }
 };
